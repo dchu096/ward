ec05c43df8a56b924276c01431a337c276740198
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-05-23 23:25:56 +1200

    Publish the HR version
    Publish HR version since HR went deleted

--- README.md
+++ README.md
@@ -1,33 +1,44 @@
-# 🔑 ward
-A Discord verification bot using hCAPTCHA. 
+# HR Warden
+A fork of nates/ward, maintained and edited for HR
 
-## 🧳 Requirements
-* node.js (v16 minimum)
+# Verication
 
-## 🔌 Installation
-```
-npm i
-```
+## Added channel sending for alerts
+![Discord_0jOXrVYpuE](https://user-images.githubusercontent.com/39256422/141204702-9d181814-a0a7-4c06-9491-b07aa143ad88.png)
 
-## 📘 Setup
-Register a site with reCAPTCHA [here](https://www.google.com/recaptcha/admin/create) and choose reCAPTCHA v2 "I'm not a robot" Checkbox. Now on your reCAPTCHA dashboard copy the secret and public key into the config.json file. Add your Discord bot token, guild ID, and role ID into config.json aswell. If you are using a domain add it in the config.json file, if you are using HTTPS, enable it in the config add your certificate and private key file with the names: `certificate.pem` and `private.pem`.
+## Verify message
+![99iE0Jwxvv](https://user-images.githubusercontent.com/39256422/141204945-3453bbe7-de00-4492-a469-db7a5f1fd41b.png)
 
-## 🕹️ Usage
-```
-npm start
-```
+## Completed verication
+![chrome_eUqQpWJoig](https://user-images.githubusercontent.com/39256422/141204987-8497d51b-0e2e-4043-8672-36b9c63a1623.png)
 
-## ❓ Issues
-### Not receiving a DM when joining my server
-If you are not receiving a DM when joining your server, Go to your Discord bot dashboard and enable both intents. Note: If your bot is more than 100 servers, you will have to verify your bot.
-### Bot failing to login
-If you have `privileged-intents` set to `true` in the config, you must go to your Discord bot dashboard and enable both intents. Note: If your bot is more than 100 servers, you will have to verify your bot.
+# Verify screen (HTML)
 
-![Intents](https://i.imgur.com/D2fDMjE.png)
+## Verify
+![chrome_J0U0dz4asF](https://user-images.githubusercontent.com/39256422/141213865-b94dea1a-49b0-4c07-9012-320494a0ed92.png)
+
+## Completed captcha screen
+![chrome_YvCygB9BDM](https://user-images.githubusercontent.com/39256422/141213894-e95637c0-7d65-4dcb-b4ac-f61843ac66da.png)
+
+# Other commands
+
+## Help menu
+![Discord_zUcsQTKr7R](https://user-images.githubusercontent.com/39256422/143380869-75b0e571-ef68-41cf-9d8f-4a67aa6fe2ee.png)
+
+## System Info
+![Discord_t159FRDPdJ](https://user-images.githubusercontent.com/39256422/143380914-742e80f5-dc9b-4bd7-9e9c-1f1a32aa73a6.png)
+
+## Ping
+![Discord_q2Jq6aCvry](https://user-images.githubusercontent.com/39256422/143380946-64b4aca3-1edc-472d-b848-9c3f22abace4.png)
 
 
-## 📷 Preview
-![Embed](https://i.imgur.com/zomEnpw.png)
-![Website](https://i.imgur.com/tmrcyjF.png)
 
-Credits: https://github.com/nates/ward
+# Forking  / maintaining
+
+Uhh... maybe just fork another build from nates/ward then here unless you really want to use the build i edited
+
+# Updates
+
+**Help menu are now in!**
+
+depends on my time but maybe adding a bypass command so the administrator can bypass the user. :wink:
--- discord.js
+++ discord.js
@@ -1,113 +1,211 @@
 // Packages
-const { Client, Intents, Discord } = require('discord.js');
+const { Client, Intents, Discord, message } = require('discord.js');
 const { Signale } = require('signale');
 const pool = require('./pool');
 const { MessageActionRow, MessageButton, MessageEmbed, Interactions } = require('discord.js');
 // Config
 const config = require('./config.json');
+const moment = require("moment");
+const os = require('os');
+const cpuStat = require("cpu-stat");
 
 // Variables
-const myIntents = new Intents();
-myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES);
-const client = new Client({ intents: myIntents });
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]
+});
 const logger = new Signale({ scope: 'Discord' });
+const prefix = config.discord['prefix'];
+
+
+
+
+
 
 // Function to start the Discord bot
 function main() {
-    logger.info('Logging in...');
+
+    console.log("\n")
+    console.log(`    :::       :::    :::    ::::::::: ::::::::: ::::::::::::::    ::: `)
+    console.log(`   :+:       :+:  :+: :+:  :+:    :+::+:    :+::+:       :+:+:   :+:  `)
+    console.log(`  +:+       +:+ +:+   +:+ +:+    +:++:+    +:++:+       :+:+:+  +:+   `)
+    console.log(` +#+  +:+  +#++#++:++#++:+#++:++#: +#+    +:++#++:++#  +#+ +:+ +#+    `)
+    console.log(`+#+ +#+#+ +#++#+     +#++#+    +#++#+    +#++#+       +#+  +#+#+#     `)
+    console.log(`#+#+# #+#+# #+#     #+##+#    #+##+#    #+##+#       #+#   #+#+#      `)
+    console.log(`###   ###  ###     ######    ############ #############    #### `)
+
     client.login(config.discord.token).catch(() => {
-        logger.fatal('Failed to login! Is your intents enabled?');
+        logger.fatal('Intents not enabled.');
         process.exit(0);
     }).then(() => {
-        logger.success('Logged in!');
+        logger.success(`Successfully logged into ${client.user.username}`);
     });
 }
 
 // New Status
 client.on('ready', () => {
     const status = config.discord['status'];
-    client.user.setActivity(status, {type: 'PLAYING'});
-    logger.success('Status Set!');
-});
-
-// Interactions
-client.on('interactionCreate', interaction => {
-// Embed define
-      const sucess = new MessageEmbed()
-          .setColor('#0099ff')
-          .setTitle('Sucess!')
-          .setDescription('You sucessfully agreed to the rules!');
-    if (!interaction.isButton()) return;
-        if (interaction.isButton("rulesa")) {
-        console.log("User agreed to rules!")
-        interaction.deferUpdate();
-	const user = client.users.cache.get(interaction.user.id);
-	user.send("You have sucessfully agreed to rules.").catch(console.error);
-    	    const linkID = pool.createLink(user.id);
-    const embed2 = new MessageEmbed()
-        .setTitle('reCAPTCHA Verification')
-        .setDescription(`To gain access to this server you must solve a captcha. The link will expire in 15 minutes.\n${config.https ? 'https://' : 'http://'}${config.domain}/verify/${linkID}`)
-        .setColor('BLUE');
-    user.send({ embeds: [embed2] }).catch(() => {
-        logger.error(`Failed to send captcha to user! (Maybe they have DMs turned off?)`);
-})
-}
+    client.user.setActivity(status, {type: 'WATCHING'});
+    logger.success('Successfully set the status');
 });
 
 // Commands
-const prefix = config.discord['prefix'];
-client.on("messageCreate", async (message) => {
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-    if(message.content.startsWith(`${prefix}ping`)){
-    	message.channel.send('Pong!');
-    }
-    if(message.content.startsWith(`${prefix}verify`)){
-    if (message.member.roles.cache.has(config.discord['verified-role-id'])) {
-        message.channel.send('Whoops, your already verified!')
-        return;
-    }
-if (config.discord['rulesenabled'] == true) {
-const row = new MessageActionRow()
-  .addComponents(
-      new MessageButton()
-          .setCustomId('rulesa')
-          .setLabel('Verify')
-          .setStyle('SUCCESS'),
-  );
-      const embed = new MessageEmbed()
-          .setColor('#0099ff')
-          .setTitle('Rules!')
-          .setURL('https://snaildos.com')
-          .setDescription(config.discord['rules']);
 
-     await message.author.send({ content: 'Rules time!', ephemeral: true, embeds: [embed], components: [row] });
-     message.channel.send("Please check your DMS!")
+
+client.on("messageCreate", async (message) => {
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    // Exit and stop if the prefix is not there or if user is a bot
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if(command === "ping"){
+
+        const pingembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${client.user.username} Ping`)
+            .addField(`Latency:`, ` ${Date.now() - message.createdTimestamp}ms.`, false)
+            .addField(`API Latency:`, `${Math.round(client.ws.ping)}ms.`, false)
+
+        message.channel.send({ embeds: [pingembed] });
     }
-} else {
-        message.channel.send('Please check your DMS!')
-    	    const linkID = pool.createLink(message.author.id);
-    const embed2 = new MessageEmbed()
-        .setTitle('reCAPTCHA Verification')
-        .setDescription(`To gain access to this server you must solve a captcha. The link will expire in 15 minutes.\n${config.https ? 'https://' : 'http://'}${config.domain}/verify/${linkID}`)
-        .setColor('BLUE');
-    message.author.send({ embeds: [embed2] }).catch(() => {
-        logger.error(`Failed to send captcha to user! (Maybe they have DMs turned off?)`);
-})}
+
+    if(command === "help"){
+
+        const pingembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${client.user.username} Help`)
+            .setDescription(`HR Warden is Hosting Remade's verify bot`)
+            .addField(`Commands`, `HR Warden Commands`, false)
+            .addField(`${prefix}help`, `List all the commands the bot have`, false)
+            .addField(`${prefix}ping`, `List the ping of the bot`, false)
+            .addField(`${prefix}system`, `List the sys info of the bot`, false)
+            .addField(`${prefix}verify`, `Verify yourself (if you havnt)`, false)
+            .addField(`${prefix}eval`, `[owner] Evaulate a code`, false)
+
+        message.channel.send({ embeds: [pingembed] });
+    }
+
+    const clean = async (text) => {
+        if (text && text.constructor.name == "Promise")
+            text = await text;
+
+        if (typeof text !== "string")
+            text = require("util").inspect(text, { depth: 1 });
+
+        text = text
+            .replace(/`/g, "`" + String.fromCharCode(8203))
+            .replace(/@/g, "@" + String.fromCharCode(8203));
+
+        return text;
+    }
+
+    if(command === "system"){
+
+        let { version } = require("discord.js");
+
+        cpuStat.usagePercent(function(err, percent, seconds) {
+            if (err) {
+                return console.log(err);
+            }
+
+            let secs = Math.floor(message.client.uptime % 60);
+            let days = Math.floor((message.client.uptime % 31536000) / 86400);
+            let hours = Math.floor((message.client.uptime / 3600) % 24);
+            let mins = Math.floor((message.client.uptime / 60) % 60);
+
+            //let duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+            let embedStats = new MessageEmbed()
+                .setTitle("System Stats")
+                .setColor("#87CEEB")
+                .addField("• CPU", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
+                .addField("• CPU usage", `\`${percent.toFixed(2)}%\``,true)
+                .addField("• Mem Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, true)
+                .addField("• Average Loads", `\`${os.loadavg()}\``,true)
+                .addField("• Uptime ", `${days}d ${hours}h ${mins}m ${secs}s`, false) //`${duration}`, true)
+                .addField("• Platform", `\`\`${os.platform()}\`\``,true)
+                .addField("• Arch", `\`${os.arch()}\``,true)
+                .addField("• Discord.js", `v${version}`, true)
+                // .addField("• Node", `${process.version}`, true)
+                .setFooter("Thats the OS that the bot runs on!")
+
+            message.channel.send({ embeds: [embedStats] }).catch(O_o => {});
+        })
+    }
+
+    if(command === "eval"){
+
+        const args2 = message.content.split(" ").slice(1);
+
+        const deniedembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`Error`)
+            .setDescription(`You are not allowed to access this command`)
+
+
+        if (message.author.id !== '420839496263925767') {
+            message.channel.send({ embeds: [deniedembed] });
+        }
+
+        try {
+            // Evaluate (execute) our input
+            const evaled = eval(args2.join(" "));
+
+            // Put our eval result through the function
+            // we defined above
+            const cleaned = await clean(evaled);
+
+            // Reply in the channel with our result
+            message.channel.send(`\`\`\`js\n${cleaned}\n\`\`\``);
+        } catch (err) {
+            // Reply in the channel with our error
+            message.channel.send(`\`ERROR\` \`\`\`xl\n${cleaned}\n\`\`\``);
+        }
+
+
+    }
+
+    if(command === "verify"){
+        if (message.member.roles.cache.has(config.discord['verified-role-id'])) {
+
+            const verifiedembed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setDescription(`You are already verified. No need to do it again!`);
+
+            await message.reply({ embeds: [verifiedembed] })
+            return;
+        } else {
+            message.channel.send('Please check your DMS!')
+            const linkID = pool.createLink(message.author.id);
+            const embed2 = new MessageEmbed()
+                .setTitle('HostingRemade Verification')
+                .setDescription(`${config.https ? 'https://' : 'http://'}${config.domain}/verify/${linkID}`)
+                .setColor('BLUE');
+            client.channels.cache.get('897370981578969138').send(`Hello ${message.member.user.tag}, welcome to HostingRemade! To participate in this server, you must verify your account. I have sent you a DM with the verification link, link will expire in 15 minutes.`)
+                .then(message.author.send({ embeds: [embed2] })).catch(() => {
+                client.channels.cache.get('897370981578969138').send(`${message.member.user.tag}, i am unable to send DMs to you. Please make sure you have DMs enabled and retry this command.`);
+            });
+        }
+
+    }
 });
 
 // Events
 // Send user the captcha when they join the server
 client.on('guildMemberAdd', member => {
+    if (member.user.bot) return;
     const linkID = pool.createLink(member.id);
-    const embed = new Discord.MessageEmbed()
-        .setTitle('reCAPTCHA Verification')
-        .setDescription(`To gain access to this server you must solve a captcha. The link will expire in 15 minutes.\n${config.https ? 'https://' : 'http://'}${config.domain}/verify/${linkID}`)
+    const embed = new MessageEmbed()
+        .setTitle('Hostingremade Verification')
+        .setDescription(`${config.https ? 'https://' : 'http://'}${config.domain}/verify/${linkID}`)
         .setColor('BLUE');
-    member.send(embed).catch(() => {
-        logger.error(`Failed to send captcha to ${member.user.tag}! (Maybe they have DMs turned off?)`);
-    });
+    try {
+        client.channels.cache.get('897370981578969138').send(`Hello ${member.user.tag}, welcome to HostingRemade! To participate in this server, you must verify your account. I have sent you a DM with the verification link, link will expire in 15 minutes.`)
+            .then(member.send({ embeds: [embed] }))
+    } catch (err) {
+        client.channels.cache.get('897370981578969138').send(`${member.user.tag}, i am unable to send DMs to you. Please make sure you have DMs enabled and retry ///verify`);
+    }
 });
+
 
 // Add verified role to user
 async function addRole(userID) {
@@ -115,13 +213,13 @@ async function addRole(userID) {
         const guild = await client.guilds.fetch(config.discord['guild-id']);
         const role = await guild.roles.fetch(config.discord['verified-role-id']);
         const member = await guild.members.fetch(userID);
-        member.roles.add(role).catch(() => {
-            logger.error(`Failed to add role to user ${member.user.tag}! (Maybe verified role is above bot role?)`);
+        member.roles.add(role).then(member.send(`You are now verified. If you still do not see the channels please DM a administrator.`)).catch(() => {
+            logger.error(`Failed to add role to user!`);
             return;
         });
-        logger.info(`Added verified role to user ${member.user.tag}.`);
+
     } catch (e) {
-        logger.error(`Failed to add role to user ${userID}!`);
+        logger.error(e);
     }
 }
 
@@ -129,26 +227,26 @@ async function addRole(userID) {
 async function removeRole(userID) {
     const ifremrole = config.discord['remove-role'];
     if (ifremrole == true) {
-    try {
-        const guild = await client.guilds.fetch(config.discord['guild-id']);
-        const remrole = await guild.roles.fetch(config.discord['remove-role-id']);
-        const member = await guild.members.fetch(userID);
+        try {
+            const guild = await client.guilds.fetch(config.discord['guild-id']);
+            const remrole = await guild.roles.fetch(config.discord['remove-role-id']);
+            const member = await guild.members.fetch(userID);
 
-        member.roles.remove(remrole).catch(() => {
-            logger.error(`Failed to remove role to user ${member.user.tag}! (Maybe role is above bot role?)`);
-            return;
-        });
-        logger.info(`Removed role to user ${member.user.tag}.`);
-    } catch (e) {
-        logger.error(`Failed to add role to user ${userID}!`);
+            member.roles.remove(remrole).catch(() => {
+                logger.error(`Failed to remove role to user ${member.user.tag}! (Maybe role is above bot role?)`);
+                return;
+            });
+            logger.info(`Removed role to user ${member.user.tag}.`);
+        } catch (e) {
+            logger.error(`Failed to add role to user ${userID}!`);
+        }
+    } else {
+        logger.info(`Remove role is set to false, step skipped.`)
     }
-   } else {
-         logger.info(`Remove role is set to false, step skipped.`)
-}
 }
 
 module.exports = {
     run: main,
     addRole,
     removeRole
-};
+}
